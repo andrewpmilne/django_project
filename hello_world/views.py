d3c7c46cc5f47984f7ab73137e6361e7aff084e2
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-06-30 17:08:02 +0100

    update views.py in hello-world to return http response data

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 # Create your views here
 
 def index(request):
-    return HttpResponse("Hello World!")
+    if request.method == "POST":
+        return HttpResponse("You have POSTed something")
+    else:
+        return HttpResponse(request.method)
